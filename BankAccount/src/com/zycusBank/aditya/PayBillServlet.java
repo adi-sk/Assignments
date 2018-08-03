8bf06e42a335e4c3f85629e3125dccca51417cc0
Aditya Shantaram Kurhade <adikrhd@gmail.com>
2018-08-03 22:06:07 +0530

    Update PayBillServlet.java

--- BankAccount/src/com/zycusBank/aditya/PayBillServlet.java
+++ BankAccount/src/com/zycusBank/aditya/PayBillServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class PayBillServlet
  */
-@WebServlet("/PayBillServlet")
+@WebServlet("/pay-bills.do")
 public class PayBillServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
